bfb4675c5f43b61b12065bda7bf341f0f83c9a67
Henry Zhang <zhangyiheng2003@gmail.com>
2024-10-21 15:36:53 -0400

    servo linear and standard driver example

--- drivers/examples/servo_example/standard.cpp
+++ drivers/examples/servo_example/standard.cpp
@@ -4,11 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    VMXChannelIndex port = 14; // servo output on pin 13
-    studica_driver::Servo servo(port, studica_driver::ServoType::Continuous, -100, 100);  
-    servo.SetAngle(100);
+    VMXChannelIndex port = 13; // servo output on pin 14
+    studica_driver::Servo servo(port, studica_driver::ServoType::Standard, -150, 150);  
+    servo.SetAngle(150);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-    servo.SetAngle(-100);
+    servo.SetAngle(-150);
     std::this_thread::sleep_for(std::chrono::seconds(1));
     servo.SetAngle(0);
     std::this_thread::sleep_for(std::chrono::seconds(1));
