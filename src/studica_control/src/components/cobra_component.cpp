52f3c1226d283e5f61263b4495f24ef012c3eb22
Hunain Atif <Hunain0132@gmail.com>
2025-03-12 16:45:35 -0400

    cobra_component update Changed cobra_component.cpp to match changes made to cobra and i2c drivers

--- src/studica_control/src/components/cobra_component.cpp
+++ src/studica_control/src/components/cobra_component.cpp
@@ -1,42 +1,33 @@
-#include "studica_control/cobra_component.h"
+#ifndef COBRA_COMPONENT_H
+#define COBRA_COMPONENT_H
+
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "cobra.h"
+#include "studica_control/srv/set_data.hpp"
+#include "VMXPi.h"
 
 namespace studica_control {
 
-Cobra::Cobra(const rclcpp::NodeOptions & options) : Node("cobra", options) {}
+class Cobra : public rclcpp::Node {
+public:
+    Cobra(std::shared_ptr<VMXPi> vmx, const std::string &name, const float& vref);
+    explicit Cobra(const rclcpp::NodeOptions & options);
+    ~Cobra();
+    void cmd_callback(std::shared_ptr<studica_control::srv::SetData::Request> request, std::shared_ptr<studica_control::srv::SetData::Response> response);
+    void cmd(std::string params, std::shared_ptr<studica_control::srv::SetData::Response> response);
 
-Cobra::Cobra(std::shared_ptr<VMXPi> vmx, const std::string &name, const float& vref, const int& muxch)
-    : Node(name), vmx_(vmx), name_(name), vref_(vref), muxch_(muxch) {
-    cobra_ = std::make_shared<studica_driver::Cobra>(vref_, muxch_, vmx_);
-    service_ = this->create_service<studica_control::srv::SetData>(
-        "cobra_cmd",
-        std::bind(&Cobra::cmd_callback, this, std::placeholders::_1, std::placeholders::_2));
-}
+private:
+    std::shared_ptr<VMXPi> vmx_;
+    std::shared_ptr<studica_driver::Cobra> cobra_;
+    rclcpp::Service<studica_control::srv::SetData>::SharedPtr service_;
+    std::string name_;
+    float vref_;
+};
 
-Cobra::~Cobra() {}
+}  // namespace studica_control
 
-void Cobra::cmd_callback(std::shared_ptr<studica_control::srv::SetData::Request> request, std::shared_ptr<studica_control::srv::SetData::Response> response) {
-    std::string params = request->params;
-    cmd(params, response);
-}
-
-void Cobra::cmd(std::string params, std::shared_ptr<studica_control::srv::SetData::Response> response) {
-    if (params == "get_raw") {
-        response->success = true;
-        response->message = std::to_string(cobra_->GetRawValue());
-    } else if (params == "get_voltage") {
-        response->success = true;
-        response->message = std::to_string(cobra_->GetVoltage());
-    } else {
-        response->success = false;
-        response->message = "No such command '" + params + "'";
-    }
-}
-
-} // namespace studica_control
-
-#include "rclcpp_components/register_node_macro.hpp"
-
-// Register the component with class_loader.
-// This acts as a sort of entry point, allowing the component to be discoverable when its library
-// is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(studica_control::Cobra)
+#endif  // COBRA_COMPONENT_H
