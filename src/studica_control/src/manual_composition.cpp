9de75fffe566d93761259f8deaed7d58533ab3d1
Andrew Shum <andrew.shum068@gmail.com>
2025-03-21 10:49:31 -0400

    refactor manual composition

--- src/studica_control/src/manual_composition.cpp
+++ src/studica_control/src/manual_composition.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
 
@@ -21,21 +22,87 @@ private:
     std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> executor_;
     std::map<std::string, std::shared_ptr<rclcpp::Node>> component_map;
 
+    std::string check_unique_name(const std::string &requested_name) {
+        std::string unique_name = requested_name;
+        int suffix = 1;
+
+        while (component_map.find(unique_name) != component_map.end()) {
+            unique_name = requested_name + "_" + std::to_string(suffix++);
+        }
+
+        if (unique_name != requested_name) {
+            RCLCPP_INFO(this->get_logger(), "Renamed component from '%s' to '%s' to ensure unique naming.",
+                requested_name.c_str(), unique_name.c_str());
+        }
+
+        return unique_name;
+    }
+
+    template<typename T, typename... Args>
+    bool create_component(const std::string &requested_name,
+                          const std::string &component_type,
+                          std::shared_ptr<studica_control::srv::SetData::Response> response,
+                          Args&&... args) {
+        try {
+            std::string unique_name = check_unique_name(requested_name);
+            auto component = std::make_shared<T>(std::forward<Args>(args)...);
+            executor_->add_node(component);
+            component_map[unique_name] = component;
+
+            response->success = true;
+            response->message = component_type + " component '" + unique_name + "' created successfully!";
+
+            RCLCPP_INFO(this->get_logger(), "Created %s component %s", component_type.c_str(), unique_name.c_str());
+
+            return true;
+        } catch (const std::exception &e) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to create %s component %s", component_type.c_str(), e.what());
+            response->success = false;
+            response->message = "Failed to create " + component_type + " component: " + std::string(e.what());
+
+            return false;
+        }
+    }
+
     void service_callback(
         const std::shared_ptr<studica_control::srv::SetData::Request> request,
         std::shared_ptr<studica_control::srv::SetData::Response> response) {
-
+        
         std::string name = request->name;
         std::string component = request->component;
-
-        if (component == "servo") {
-            uint8_t pin = request->initparams.pin;
-            std::string servo_type_str = request->initparams.servo_type;
-            std::string name = request->name;
-
+        auto ip = request->initparams;
+        
+        if (component == "cobra") {
+            create_component<studica_control::Cobra>(name, "Cobra", response, vmx_, "Cobra", ip.vref);
+        } else if (component == "diffdrive") {
+            std::string unique_name = check_unique_name("diff_odom");
+            auto component = std::make_shared<studica_control::DiffOdometry>();
+            executor_->add_node(component);
+            component_map[unique_name] = component;
+            create_component<studica_control::DiffDrive>(
+                name, "Differential Drive", response, vmx_, component, "DiffDrive", ip.can_id, ip.motor_freq, 
+                ip.ticks_per_rotation, ip.wheel_radius, ip.wheel_separation, ip.left, ip.right, 
+                ip.invert_left, ip.invert_right);
+        } else if (component == "dio") {
+            create_component<studica_control::DIO>(
+                name, "DIO", response, vmx_, ip.pin, studica_driver::PinMode::OUTPUT, std::string(request->params));
+        } else if (component == "encoder") {
+            create_component<studica_control::Encoder>(name, "Encoder", response, vmx_, "Encoder", ip.port_a, ip.port_b);
+        } else if (component == "imu") {
+            create_component<studica_control::Imu>(name, "IMU", response, vmx_);
+        } else if (component == "mecanum") {
+            create_component<studica_control::MecanumDrive>(
+                name, "Mecanum Drive", response, vmx_, "MecanumDrive", ip.can_id, ip.motor_freq,
+                ip.ticks_per_rotation, ip.wheel_radius, ip.wheelbase, ip.width, ip.front_left,
+                ip.front_right, ip.rear_left, ip.rear_right, ip.invert_front_left, ip.invert_front_right,
+                ip.invert_rear_left, ip.invert_rear_right);
+        } else if (component == "servo") {
+            uint8_t pin = ip.pin;
+            std::string servo_type_str = ip.servo_type;
+            
             studica_driver::ServoType servo_type;
             int min_angle = 0, max_angle = 0;
-            
+
             if (servo_type_str == "standard") {
                 servo_type = studica_driver::ServoType::Standard;
                 min_angle = -150;
@@ -54,212 +121,38 @@ private:
                 response->message = "Invalid servo type.";
                 return;
             }
-
-            try {
-                auto servo_node = std::make_shared<studica_control::Servo>(vmx_, name, pin, servo_type, min_angle, max_angle);
-                executor_->add_node(servo_node);
-                component_map[name] = servo_node;
-
-                response->success = true;
-                response->message = "Servo component '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created servo component '%s' of type '%s' on pin %d.",
-                            name.c_str(), servo_type_str.c_str(), pin);
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create servo component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create servo component: " + std::string(e.what());
-            }
+            create_component<studica_control::Servo>(name, "Servo", response, vmx_, name, pin, servo_type, min_angle, max_angle);
         } else if (component == "sharp") {
-            try {
-                auto ip = request->initparams;
-                auto sharp_node = std::make_shared<studica_control::Sharp>("sharp", ip.port, vmx_);
-                executor_->add_node(sharp_node);
-                component_map[name] = sharp_node;
-
-                response->success = true;
-                response->message = "Sharp '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created Sharp component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create differential drive component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create differential drive component: " + std::string(e.what());
-            }
-        
-        } else if (component == "cobra") {
-            try {
-                auto ip = request->initparams;
-                auto cobra_node = std::make_shared<studica_control::Cobra>(
-                    vmx_, "Cobra", 
-                    ip.vref);
-                executor_->add_node(cobra_node);
-                component_map[name] = cobra_node;
-
-                response->success = true;
-                response->message = "Cobra '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created Cobra component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create Cobra component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create Cobra component: " + std::string(e.what());
-            }
-        } else if (component == "encoder") {
-            try {
-                auto ip = request->initparams;
-                auto encoder_node = std::make_shared<studica_control::Encoder>(
-                    vmx_, "Encoder", 
-                    ip.port_a, 
-                    ip.port_b);
-                executor_->add_node(encoder_node);
-                component_map[name] = encoder_node;
-
-                response->success = true;
-                response->message = "Encoder '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created Encoder component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create Encoder component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create Encoder component: " + std::string(e.what());
-            }
-        } else if (component == "imu") {
-            try {
-                auto imu_node = std::make_shared<studica_control::Imu>(vmx_);
-                executor_->add_node(imu_node);
-                component_map[name] = imu_node;
-
-                response->success = true;
-                response->message = "IMU component '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created IMU component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create IMU component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create IMU component: " + std::string(e.what());
-            }
+            create_component<studica_control::Sharp>(name, "Sharp", response, "sharp", ip.port, vmx_);
         } else if (component == "titan") {
-            try {
-                auto ip = request->initparams;
-                auto titan_node = std::make_shared<studica_control::Titan>(
-                    vmx_, "Titan", 
-                    ip.can_id, 
-                    ip.motor_freq, 
-                    ip.ticks_per_rotation,
-                    ip.wheel_radius);
-                executor_->add_node(titan_node);
-                component_map[name] = titan_node;
-
-                response->success = true;
-                response->message = "Titan '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created Titan component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create Titan component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create Titan component: " + std::string(e.what());
-            }
-        } else if (component == "dio") {
-            try {
-                auto ip = request->initparams;
-                auto dio_node = std::make_shared<studica_control::DIO>(vmx_, ip.pin, studica_driver::PinMode::OUTPUT, std::string(request->params));
-                executor_->add_node(dio_node);
-                component_map[name] = dio_node;
-
-                response->success = true;
-                response->message = "DIO component '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created DIO component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create DIO component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create DIO component: " + std::string(e.what());
-            }
-        } else if (component == "diffdrive") {
-            try {
-                auto ip = request->initparams;
-                auto diff_node = std::make_shared<studica_control::DiffDrive>(
-                    vmx_, "DiffDrive", 
-                    ip.can_id, 
-                    ip.motor_freq, 
-                    ip.ticks_per_rotation, 
-                    ip.wheel_radius, 
-                    ip.wheel_separation,
-                    ip.left,
-                    ip.right,
-                    ip.invert_left,
-                    ip.invert_right);
-                executor_->add_node(diff_node);
-                component_map[name] = diff_node;
-
-                response->success = true;
-                response->message = "Differential Drive Controller '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created DiffDrive component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create differential drive component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create differential drive component: " + std::string(e.what());
-            }
-        } else if (component == "mecanum") {
-            try {
-                auto ip = request->initparams;
-                auto mecanum_node = std::make_shared<studica_control::MecanumDrive>(
-                    vmx_, "MecanumDrive", 
-                    ip.can_id, 
-                    ip.motor_freq, 
-                    ip.ticks_per_rotation, 
-                    ip.wheel_radius, 
-                    ip.wheelbase, 
-                    ip.width,
-                    ip.front_left,
-                    ip.front_right,
-                    ip.rear_left,
-                    ip.rear_right,
-                    ip.invert_front_left,
-                    ip.invert_front_right,
-                    ip.invert_rear_left,
-                    ip.invert_rear_right);
-                executor_->add_node(mecanum_node);
-                component_map[name] = mecanum_node;
-
-                response->success = true;
-                response->message = "Mecanum Drive Controller '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created MecanumDrive component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create mecanum drive component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create mecanum drive component: " + std::string(e.what());
-            }
+            create_component<studica_control::Titan>(
+                name, "Titan", response, vmx_, "Titan", ip.can_id, ip.motor_freq,
+                ip.ticks_per_rotation, ip.wheel_radius);
         } else if (component == "ultrasonic") {
-            try {
-                auto ip = request->initparams;
-                auto ultrasonic_node = std::make_shared<studica_control::Ultrasonic>(vmx_, "ultrasonic", ip.ping, ip.echo);
-                executor_->add_node(ultrasonic_node);
-                component_map[name] = ultrasonic_node;
-
-                response->success = true;
-                response->message = "Ultrasonic component '" + name + "' created successfully.";
-                RCLCPP_INFO(this->get_logger(), "Created Ultrasonic component '%s'.", name.c_str());
-            } catch (const std::exception &e) {
-                RCLCPP_ERROR(this->get_logger(), "Failed to create Ultrasonic component: %s", e.what());
-                response->success = false;
-                response->message = "Failed to create Ultrasonic component: " + std::string(e.what());
-            }
+            create_component<studica_control::Ultrasonic>(
+                name, "Ultrasonic", response, vmx_, "ultrasonic", ip.ping, ip.echo);
         } else {
             RCLCPP_WARN(this->get_logger(), "Invalid component type '%s'.", component.c_str());
             response->success = false;
-            response->message = "Invalid component type. Allowed type is 'servo'.";
+            response->message = "Invalid component type.";
         }
     }
-
+    
 public:
     std::shared_ptr<VMXPi> vmx_;
+
     ControlServer() : Node("control_server") {
         vmx_ = std::make_shared<VMXPi>(true, 50);
 
         service_ = this->create_service<studica_control::srv::SetData>(
-            "create_component", 
+            "create_component",
             std::bind(&ControlServer::service_callback, this, std::placeholders::_1, std::placeholders::_2));
     }
-    void set_executor(const std::shared_ptr<rclcpp::executors::MultiThreadedExecutor>& exec) {
+
+    void set_executor(const std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> &exec) {
         executor_ = exec;
     }
 };
-
 
 int main(int argc, char *argv[]) {
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
@@ -270,7 +163,7 @@ int main(int argc, char *argv[]) {
     auto control_server = std::make_shared<ControlServer>();
     control_server->set_executor(executor);
     executor->add_node(control_server);
-    
+
     executor->spin();
 
     rclcpp::shutdown();
