e2c64a73e70c81589df5e69b3fcf048af8429015
Matthew0803 <matthewfc83@gmail.com>
2025-09-12 10:53:38 -0400

    fixed launch file to include param

--- src/studica_control/launch/studica_launch.py
+++ src/studica_control/launch/studica_launch.py
@@ -9,12 +9,20 @@ def generate_launch_description():
     pkg_share = get_package_share_directory('studica_control')
     params_file = os.path.join(pkg_share, 'config', 'params.yaml')
  
+    with open(params_file, 'r') as f:
+        params = yaml.safe_load(f)
+
+    # The manual_composition node is a container for components.
+    # We need to pass the parameters for each component to the container.
+    # The container will then pass them to the correct component upon loading.
+    # The parameters in the YAML are nested under component names, 
+    # so we load the whole file as parameters for the container node.
     manual_composition = Node(
         package='studica_control',
         executable='manual_composition',
         name='control_server',
         output='screen',
-        parameters=[params_file]
+        parameters=[params]
     )
 
     nodes = [
