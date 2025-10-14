49bd018d5a01a131f273a0fe0202b17ad5e8a973
Matthew0803 <matthewfc83@gmail.com>
2025-10-14 12:25:32 -0400

    disable gamepad_component

--- src/studica_control/src/manual_composition.cpp
+++ src/studica_control/src/manual_composition.cpp
@@ -9,7 +9,7 @@
 #include "studica_control/diff_drive_component.h"
 #include "studica_control/dio_component.h"
 #include "studica_control/encoder_component.h"
-#include "studica_control/gamepad_component.h"
+//#include "studica_control/gamepad_component.h"
 #include "studica_control/imu_component.h"
 #include "studica_control/mecanum_drive_component.h"
 #include "studica_control/servo_component.h"
@@ -37,7 +37,7 @@ public:
         this->declare_parameter<bool>("diff_drive_component.enabled", false);
         this->declare_parameter<bool>("dio.enabled", false);
         this->declare_parameter<bool>("encoder.enabled", false);
-        this->declare_parameter<bool>("gamepad.enabled", false);
+        //this->declare_parameter<bool>("gamepad.enabled", false);
         this->declare_parameter<bool>("imu.enabled", false);
         this->declare_parameter<bool>("mecanum_drive_component.enabled", false);
         this->declare_parameter<bool>("servo.enabled", false);
@@ -55,7 +55,7 @@ public:
         bool diff_drive_enabled = this->get_parameter("diff_drive_component.enabled").as_bool();
         bool dio_enabled = this->get_parameter("dio.enabled").as_bool();
         bool encoder_enabled = this->get_parameter("encoder.enabled").as_bool();
-        bool gamepad_enabled = this->get_parameter("gamepad.enabled").as_bool();
+        //bool gamepad_enabled = this->get_parameter("gamepad.enabled").as_bool();
         bool imu_enabled = this->get_parameter("imu.enabled").as_bool();
         bool mecanum_drive_enabled = this->get_parameter("mecanum_drive_component.enabled").as_bool();
         bool servo_enabled = this->get_parameter("servo.enabled").as_bool();
@@ -95,10 +95,10 @@ public:
             component_nodes.insert(component_nodes.end(), encoder_nodes.begin(), encoder_nodes.end());
         }
 
-        if (gamepad_enabled) {
-            auto gamepad_node = studica_control::GamepadController::initialize(this);
-            component_nodes.push_back(gamepad_node);
-        }
+        //if (gamepad_enabled) {
+        //    auto gamepad_node = studica_control::GamepadController::initialize(this);
+        //    component_nodes.push_back(gamepad_node);
+        //}
 
         if (imu_enabled) {
             auto imu_node = studica_control::Imu::initialize(this, vmx_);
